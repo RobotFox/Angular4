faa1034d172ee9eb9ce9617c5f59e5845154c93a
RobotFox <samuele.pretto@gmail.com>
2017-09-22 17:30:34 +0200

    Commit example

---
+++ AngularComponents/src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import {Component } from '@angular/core';
+
+@Component({
+  // in the html we call the component <courses>
+  selector:'courses',
+  template:'<h2>Courses</h2>'
+})
+export class CoursesComponent {
+
+}
